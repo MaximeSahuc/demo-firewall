4ee8e2e998945fc27530cb0d038b330457b81951
marccambon <marccambonm@gmail.com>
2025-03-11 11:58:35 +0100

    change rules ufw

--- sae-Firewall/firewall/provision/setup_ufw.sh
+++ sae-Firewall/firewall/provision/setup_ufw.sh
@@ -7,8 +7,18 @@ echo "y" | sudo ufw enable
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
-# Allow SSH
-sudo ufw allow "OpenSSH"
 
-# Enable Nginx profile (allow 80:433)
-sudo ufw allow "Nginx HTTP"
+#Allow Host/VM trafic
+sudo ufw allow in on eth0
+sudo ufw allow out on eth0
+
+# Allow SSH from LAN
+sudo ufw allow from 192.168.56.0/24 proto tcp to any port 22
+
+# Allow HTTP & HTTPS from WAN
+sudo ufw allow in on eth2 to 192.168.56.20 port 80
+sudo ufw allow in on eth2 to 192.168.56.20 port 443
+
+# Allow LAN to WAN trafic 
+sudo ufw allow in on eth1 to 192.168.57.0/24
+sudo ufw allow in on eth1 to 192.168.57.0/24
