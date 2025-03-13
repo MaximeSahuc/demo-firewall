bb1e6a1ca6c9d53a27c591f25a272aa49b4973ba
marccambon <marccambonm@gmail.com>
2025-03-13 14:33:25 +0100

    fixed auto icmp

--- sae-Firewall/provision/firewall/setup_ufw.sh
+++ sae-Firewall/provision/firewall/setup_ufw.sh
@@ -1,7 +1,12 @@
 sudo apt update && sudo apt install -yy ufw
 
+# Comment the default ACCEPT ICMP rule 
+sudo sed -i '/-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT/s/^/# /' /etc/ufw/before.rules
+sudo sed -i '/-A ufw-before-forward -p icmp --icmp-type echo-request -j ACCEPT/s/^/# /' /etc/ufw/before.rules
+
 # Enable firewall
 echo "y" | sudo ufw enable
+
 
 # Default rules
 sudo ufw default deny incoming
@@ -12,18 +17,11 @@ sudo ufw default deny routed
 #Allow Host/VM trafic
 sudo ufw allow in on eth0 proto tcp to any port 22
 
-# Allow SSH from LAN
-# sudo ufw allow  in on eth1 from 192.168.56.0/24 proto tcp to any port 22
-
-# Allow HTTP & HTTPS from WAN
-# sudo ufw allow in on eth2 to 192.168.56.20 port 80 proto tcp
-# sudo ufw allow in on eth2 to 192.168.56.20 port 443 proto tcp
-
-# Allow LAN to WAN trafic 
-sudo ufw allow in on eth1 to 192.168.57.0/24
-
 # Allow all routed traffic to web server
 sudo ufw route allow to 192.168.56.20 port 80 proto tcp
 
 # Allow all routed to WAN
 sudo ufw route allow in on eth1 to any
+
+# Reload UFW
+sudo ufw reload
